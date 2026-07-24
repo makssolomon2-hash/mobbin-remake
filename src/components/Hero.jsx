0f8f68b384001e873e80a458bd1faffea42f21dd
Maks Solomon <maks.solomon2@gmail.com>
2026-07-24 23:05:06 +0200

    proper sizing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,16 +8,16 @@ import teams from "./ui/teams.png"
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen center overflow-hidden">
+    <section className="relative  -mt-30 w-full h-screen center overflow-hidden">
      
 
       {/* Everything here sits above the background */}
       <div className="relative z-10 flex flex-col w-full h-full">
        {/*  <NavBar /> */}
         <section className="relative h-screen p-5 w-full flex items-center justify-center -z-1">
-          <div className="hero-container text-center m-10">
+          <div className="hero-container w-[950px] text-center m-10">
             <div className="flex justify-center mb-4">
-              <img src={logos} alt="" className="w-30 h-30 mt-30" /> {/* Create LogoCarousel.jsx in ui with GSAP */}
+              <img src={logos} alt="" className="w-27 h-27 mt-0" /> {/* Create LogoCarousel.jsx in ui with GSAP */}
             </div>
 
             <h1 className="text-8xl font-bold text-black mt-10 ">Discover real-world design inspiration.</h1>
@@ -28,7 +28,7 @@ const Hero = () => {
             <button className="rounded-full  m-5 p-3 px-7  bg-blue-500 text-white text-2xl font-medium cursor-pointer">Join for free</button>
             <button className="rounded-full p-3 px-7  bg-white text-black border-1 text-2xl font-medium cursor-pointer">See our plans →</button>
 
-            <p className="mt-20 text-xl font-medium">Trusted by design teams at</p>
+            <p className="mt-10 text-xl font-medium">Trusted by design teams at</p>
             <div className="teams-logos text-center flex items-center justify-center mt-10 ">
               <img src={teams} alt="" ></img>
               <img src={''} alt=""></img>
--- src/components/JoinVideo.jsx
+++ src/components/JoinVideo.jsx
@@ -5,11 +5,11 @@ import joinVideo from '../assets/video/video_hero.mp4';
 
 const JoinVideo = () => {
   return (
-    <section className="flex justify-center items-center mb-20">
+    <section className="relative  z-10 -mt-60 flex justify-center items-center mb-20">
       
-      <div className="p-[80px] bg-gray-200 rounded-3xl">
+      <div className="video-container  p-[80px] bg-gray-200 rounded-3xl">
         <video
-          className="w-full h-[865px] object-cover rounded-3xl"
+          className="w-full h-[995px] object-cover rounded-3xl"
           autoPlay
           muted
           loop

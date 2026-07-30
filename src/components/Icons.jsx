78a06fe7453a219c0bff82e8913aa2de28f86903
Maks Solomon <maks.solomon2@gmail.com>
2026-07-30 18:49:53 +0200

    fix

---
+++ src/components/Icons.jsx
@@ -0,0 +1,20 @@
+/*
+import React from 'react'
+import performanceImages from 'src/constants/index.js';
+
+const Icons = () => {
+    return (
+        <div className="wrapper">
+            {performanceImages.map((item, index ) => (
+                <img
+                    key={index}
+                    src={item.src}
+                    className={item.id}
+                    alt={item.alt || `Performance Image #${index + 1}`}
+                />
+            ))}
+        </div>
+    )
+}
+export default Icons
+*/
